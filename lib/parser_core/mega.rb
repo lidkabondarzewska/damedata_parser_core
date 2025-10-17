f4a974f1e717ff756b56730b4c42823c144fc1e3
lidkabondarzewska <lidkabondarzewska@gmail.com>
2025-10-17 12:00:19 +0200

    change to bind_call(self)

--- lib/parser_core/mega.rb
+++ lib/parser_core/mega.rb
@@ -4,7 +4,7 @@ module ParserCore
       called_from = caller_locations[0].label
       included_modules = (self.class.included_modules - Class.included_modules - [Mega])
       Rails.logger.debug("zmienione wywolanie")
-      included_modules.map { |m| m.instance_method(called_from).bind(self).call }
+      included_modules.map { |m| m.instance_method(called_from).bind_call(self) }
     end
   end
 end
