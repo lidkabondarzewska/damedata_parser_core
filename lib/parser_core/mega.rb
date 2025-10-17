e38e48b14cc55b0370c8398afd65c6d401f04417
lidkabondarzewska <lidkabondarzewska@gmail.com>
2025-10-17 11:59:18 +0200

    debug logger

--- lib/parser_core/mega.rb
+++ lib/parser_core/mega.rb
@@ -3,8 +3,8 @@ module ParserCore
     def mega
       called_from = caller_locations[0].label
       included_modules = (self.class.included_modules - Class.included_modules - [Mega])
-      included_modules.map { |m| m.instance_method(called_from).bind(self).call }
       Rails.logger.debug("zmienione wywolanie")
+      included_modules.map { |m| m.instance_method(called_from).bind(self).call }
     end
   end
 end
