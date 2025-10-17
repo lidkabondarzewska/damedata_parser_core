608dc44e2616b51acdd9e34188a5c68de0319bae
lidkabondarzewska <lidkabondarzewska@gmail.com>
2025-10-17 11:38:39 +0200

    refactor: bind(self) to bind_call(self)

--- lib/parser_core/mega.rb
+++ lib/parser_core/mega.rb
@@ -3,7 +3,7 @@ module ParserCore
     def mega
       called_from = caller_locations[0].label
       included_modules = (self.class.included_modules - Class.included_modules - [Mega])
-      included_modules.map { |m| m.instance_method(called_from).bind(self).call }
+      included_modules.map { |m| m.instance_method(called_from).bind_call(self) }
     end
   end
 end
